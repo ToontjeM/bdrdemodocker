c1b4a9b5099c7e084899fbd6c245ae6eb2206173
borysneselovskyi <70379260+borysneselovskyi@users.noreply.github.com>
2022-09-02 09:59:50 +0200

    Update demo_prep.sh

--- demo_prep.sh
+++ demo_prep.sh
@@ -8,7 +8,7 @@ BIN_DIR=${DEMO_DIR}/scripts		; export BIN_DIR
 i=pg1
 scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/data.sql admin@${i}:/home/admin
 scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/pg1.sh admin@${i}:/home/admin
-scp -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/data.sql"
+ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/data.sql"
 ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/pg1.sh"
 ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "/home/admin/pg1.sh"
 
