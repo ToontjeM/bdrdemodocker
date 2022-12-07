2d2514963217625a69b62a947834231440a78f5d
Ton Machielsen--replace-all <ton@machielsen.net>
2022-12-07 13:41:58 +0100

    docker updates

--- demo_prep.sh
+++ demo_prep.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
+primary1=upwards
+primary2=koala
+witness=unpaid
 
 # Environment variables
 DEMO_DIR=`pwd`				; export DEMO_DIR
-#DEMO_DIR="/root/projects/efmdemo"	; export DEMO_DIR
 BIN_DIR=${DEMO_DIR}/scripts		; export BIN_DIR
 
-i=pg1
-scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/data.sql admin@${i}:/home/admin
-scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/pg1.sh admin@${i}:/home/admin
-ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/data.sql"
-ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/pg1.sh"
-ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "/home/admin/pg1.sh"
+# Primary
+docker cp ${BIN_DIR}/data.sql ${primary1}:/home/admin
+docker cp ${BIN_DIR}/pg1.sh ${primary1}:/home/admin
+docker exec -ti ${primary1} "chmod 755 /home/admin/pg1.sh"
+docker exec -ti ${primary1} "chmod 755 /home/admin/data.sql"
+docker exec -ti ${primary1} "/home/admin/pg1.sh"
 
-i=pg3
-scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/pg3.sh admin@${i}:/home/admin
-ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/pg3.sh"
-ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "/home/admin/pg3.sh"
+# Witness
+docker cp ${BIN_DIR}/pg3.sh ${witness}:/home/admin
+docker exec -ti ${witness} "chmod 755 /home/admin/pg3.sh"
+docker exec -ti ${witness} "/home/admin/pg3.sh"
 
-for i in `echo "pg1 pg2"`
+# Primaries
+for i in [${primary1},${primary2}]
 do
   echo ${i}
-  scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/remote_ctl.sh admin@${i}:/home/admin
-  scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/efm_rewind.sh admin@${i}:/home/admin
-  scp -F ${DEMO_DIR}/ssh_config ${BIN_DIR}/efm_reconfigure_node.sh admin@${i}:/home/admin
-  ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "chmod 755 /home/admin/remote_ctl.sh"
-  ssh -F ${DEMO_DIR}/ssh_config admin@${i} -t "/home/admin/remote_ctl.sh"
+  docker cp ${BIN_DIR}/remote_ctl.sh ${i}:/home/admin
+  docker cp ${BIN_DIR}/efm_rewind.sh ${i}:/home/admin
+  docker cp ${BIN_DIR}/efm_reconfigure_node.sh ${i}:/home/admin
+  docker exec -ti ${i} "chmod 755 /home/admin/remote_ctl.sh"
+  docker exec -ti ${i} "/home/admin/remote_ctl.sh"
 done
